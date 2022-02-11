086dbb4555c2924399029a429b9efbc7be26ad8a
netonblpb <skullbog@gmail.com>
2022-02-11 14:49:19 +0300

    Task25 complete

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти сумму чисел от 1 до А
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+int sum = 0;
+int i = 1;
+while (i <= A)
+{
+    sum += i;
+    i++;
+}
+
+Console.WriteLine($"Сумма чисел от 1 до {A}: {sum}");
